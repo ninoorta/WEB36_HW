aadc8c60c2932a4b60f9ce630c2a86b53f216a1e
ninoorta <blackcathp9x@gmail.com>
2020-02-19 23:41:30 +0700

    commit day4 HW

---
+++ Day4_HW/app.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/Ninoorta/' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'index/html' });
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+/*
+        http://localhost:8080
+*/
