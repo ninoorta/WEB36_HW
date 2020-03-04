09333bd986f8aae47a4366ce4c3388e74acc3265
Ninoorta <blackcathp9x@gmail.com>
2020-03-04 17:44:20 +0700

    finish hw ss6

--- Day6_HW/app.js
+++ Day6_HW/app.js
@@ -1,24 +1,74 @@
-$('document').ready(function(){
-    $.ajax({url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-    ,method: 'GET'
-    ,success: function(data){
-        let transformData = JSON.parse(data)
-        let dataLength = transformData.quotes.length
-        let ranDomNumber = Math.floor(Math.random() * dataLength)
-        let randomQuote = transformData.quotes[ranDomNumber].quote
-        let thisRandomAuthor = transformData.quotes[ranDomNumber].author
+$('document').ready(function () {
+    $.ajax({
+        url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+        , method: 'GET'
+        , success: function (data) {
+            let transformData = JSON.parse(data)
+            let dataLength = transformData.quotes.length
+            let randomNumber = Math.floor(Math.random() * dataLength)
+        
+            let color = getRandomColor()
 
-        $('div').css({"background-color":`#fff`})
+            $('div').css(
+            { "background-color": `#fff`,
+              "margin": "0 auto",
+              "width": "550px",
+              "padding": "40px"
+            
+            })
+            $('h1').text(transformData.quotes[randomNumber].quote)
+            $('h2').text(transformData.quotes[randomNumber].author)
+            $('body').css(
+                {
+                    "background": color,
+                    "color": color
+                })
+            $('button').css({
+                "color": '#fff',
+                "border-radius": '5px',
+                "margin": "10px",
+                "padding": "10px",
+                "background":color
+            })
 
-        $('h1').text(randomQuote)
-        $('h2').text(thisRandomAuthor)
-        $('body').css(
-        {
-            "background": `rgb(${ranDomNumber},${ranDomNumber*2},${ranDomNumber/2})`
-            ,"color": `rgb(${ranDomNumber},${ranDomNumber+50},${ranDomNumber+22})`
-        })
+            $('button').click(function () {
+                let newColor = getRandomColor()
+                randomNumber++;
+                let red = randomNumber+20;
+                let green = randomNumber+30;
+                let blue = randomNumber+10;
 
-     
-      }});
+                $('button').css({
+                    "background": newColor,
+                })
+               
+                $('h1').text(transformData.quotes[randomNumber].quote)
+                $('h2').text(transformData.quotes[randomNumber].author)
+                $('body').css(
+                    {
+                        "background": newColor,
+                        "color": newColor
+                    })
+
+                console.log(randomNumber)
+            })
+
+
+
+        }
+    });
+
+
 
 })
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
