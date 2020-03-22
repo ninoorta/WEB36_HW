827a9d0e1a5382df02dda737f43ae11ad1cd47e2
ninoorta <blackcathp9x@gmail.com>
2020-03-22 23:42:48 +0700

    push weather web (almost finished)

--- Weather Web/app.js
+++ Weather Web/app.js
@@ -1,20 +1,55 @@
 $('document').ready(function () {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?id=1581298&appid=9cc747ed3457f77c3b4daf1950e83e8e"
-        , success: function (data) {
-            console.log(data)
-            // $("#div1").text(data.weather[0].main);
-            var icons = new Skycons({ "color": "orange" });
-            // you can add a canvas by it's ID...
-            icons.add("clear-day", Skycons.PARTLY_CLOUDY_DAY);
-            icons.set("rain", Skycons.RAIN);
+
+  
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+
+            $.ajax({
+                url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9cc747ed3457f77c3b4daf1950e83e8e`
+                , success: function (data) {
+                    
+                    let weatherConditions = [];
+
+                    $("#current-temp").text(Math.round((data.main.temp)/10) +'°C');
+                    $("#location-header").text(data.name + ', ' + data.sys.country);
+                    
 
 
-            icons.play();
-        }
-    });
+                    console.log(data)
+                    // $("#div1").text(data.weather[0].main);
+                    var icons = new Skycons({ "color": "orange" });
+                    // you can add a canvas by it's ID...
+                    icons.add("clear-day", Skycons.PARTLY_CLOUDY_DAY);
+                    icons.set("rain", Skycons.RAIN);
+                    icons.add("weather-icon", Skycons.PARTLY_CLOUDY_DAY)
+        
+                    icons.play();
+                }
+            });
+        });
 
-    // var skycons = new Skycons({ "color": "pink" });
-    // skycons.add("icon", Skycons.PARTLY_CLOUDY_DAY);
+
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+
+
     
 })
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(layToaDo);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function layToaDo(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+}
